25b5357571da8611022b37095c3d3d054083cd71
Pawel Borawski <ztjboro@gmail.com>
2021-06-07 20:53:40 -0400

    added boarding a passenger method added listing passengers names method added operator methods #[] and #<<

--- lib/flight.rb
+++ lib/flight.rb
@@ -13,4 +13,22 @@ end
 def full?
     @passengers.length >= @capacity ? true : false
 end
+
+def board_passenger(passenger)
+    if !self.full? && passenger.has_flight?(@flight_number) 
+        @passengers << passenger
+    end
+end
+
+def list_passengers
+    @passengers.map{|passenger| passenger.name}
+end
+
+def [](index)
+    @passengers[index]
+end
+
+def <<(passenger)
+    self.board_passenger(passenger)
+end
 end
